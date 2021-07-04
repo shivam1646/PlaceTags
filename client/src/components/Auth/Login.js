b4bd39225000cb51e0ef12a0682f0131920afd41
shivam1646 <shivamkotak16.sk@gmail.com>
2021-07-05 00:08:16 +0530

    PlaceTags - Tag your favorite places, add reviews, pictures, share with friends

---
+++ client/src/components/Auth/Login.js
@@ -0,0 +1,65 @@
+import React, {useContext} from "react";
+import { GraphQLClient } from 'graphql-request';
+import { GoogleLogin } from "react-google-login";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
+import Context from "../../context";
+import { ME_QUERY } from "../../graphql/queries";
+import { BASE_URL } from "../../client";
+
+const Login = ({ classes }) => {
+  const { dispatch } = useContext(Context);
+  const onSuccess = async googleUser => {
+    try {
+      const idToken = googleUser.getAuthResponse().id_token;
+      const client = new GraphQLClient(BASE_URL, {
+        headers: { authorization: idToken}
+      });
+      const { me } = await client.request(ME_QUERY);
+      dispatch({ type: "LOGIN_USER", payload: me });
+      dispatch({ type: "IS_LOGGED_IN", payload: googleUser.isSignedIn() });
+    } catch (error) {
+      onFailure(error);
+    }
+  }
+
+  const onFailure = err => {
+    console.error("error logging in");
+    dispatch({ type: "IS_LOGGED_IN", payload: false });
+  }
+
+  return (
+    <div className={classes.root}>
+      <Typography
+        component="h1"
+        variant="h3"
+        gutterBottom
+        noWrap
+        style={{ color: "rgb(66, 133, 244)" }}
+      >
+        Welcome
+      </Typography>
+      <GoogleLogin
+        clientId="551477944686-r7cvqh6hhie0o7qt9vicupe2un5kmq1b.apps.googleusercontent.com"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        isSignedIn={true}
+        buttonText="Login with Google"
+        theme="dark"
+      />
+    </div>
+  )
+};
+
+const styles = {
+  root: {
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center"
+  }
+};
+
+export default withStyles(styles)(Login);
